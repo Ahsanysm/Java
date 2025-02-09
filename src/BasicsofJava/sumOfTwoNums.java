5faabb743abe3bfe7141d48bf80f56b4513f064a
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-09 18:51:19 +0530

    Basics of JAVA

---
+++ src/BasicsofJava/sumOfTwoNums.java
@@ -0,0 +1,4 @@
+package BasicsofJava;
+
+public class sumOfTwoNums {
+}
---
+++ src/TakingInput/ark.java
@@ -0,0 +1,7 @@
+package TakingInput;
+
+public class ark {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
