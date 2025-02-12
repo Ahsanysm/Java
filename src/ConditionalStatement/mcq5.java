f5507d98f148b56c6db64d8fac771f76ae8256c9
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-12 23:21:30 +0530

    mcqs with results

---
+++ src/ConditionalStatement/mcq5.java
@@ -0,0 +1,14 @@
+package ConditionalStatement;
+
+public class mcq5 {
+    public static void main(String[] args) {
+        int i=65;
+        char j='A';
+        if(i==j)
+            System.out.println("java is wow");
+        else
+            System.out.println("java is headache");
+    }
+}
+//here i==j because compiler printed the ascii valur of A
+//which is 65 thus i==j here
