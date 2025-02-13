7ec971eb1131e8dafd3b5192d6e7f5e8b86f3eb0
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-13 23:29:21 +0530

    Decrement in Loops exercise

---
+++ src/MasteringLoops/ReverseLoop.java
@@ -0,0 +1,9 @@
+package MasteringLoops;
+//print number from 10-1 using descrement operator
+public class ReverseLoop {
+    public static void main(String[] args) {
+        for (int i = 10; i >=1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
