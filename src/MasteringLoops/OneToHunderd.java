b912c919503aecd8bbb2cc1bb5bc35afd6b5d9f7
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-13 17:59:52 +0530

    Number 1 to 100

---
+++ src/MasteringLoops/OneToHunderd.java
@@ -0,0 +1,10 @@
+package MasteringLoops;
+//print 1-100 numbers
+public class OneToHunderd {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
