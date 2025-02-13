d63fd8033761f50ea2baea9a950847aaf1c52063
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-13 23:15:09 +0530

    Table

---
+++ src/MasteringLoops/NinteenTable.java
@@ -0,0 +1,9 @@
+package MasteringLoops;
+//print table of 19
+public class NinteenTable {
+    public static void main(String[] args) {
+        for (int i = 19; i <=190 ; i=i+19) {
+            System.out.println(i);
+        }
+    }
+}
