fb8b90e3a75173fac6fdaf08d1e51f302a31c821
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-14 19:11:30 +0530

    GP

---
+++ src/MasteringLoops/GP.java
@@ -0,0 +1,15 @@
+package MasteringLoops;
+//display the gp 1,2,4,8,32 upto n terms where n is users input
+import java.util.Scanner;
+public class GP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of terms you want to print");
+        int n = sc.nextInt();
+        int a = 1, r = 2;
+        for (int i = 1; i <n ; i++) {
+            System.out.println(a+" ");
+            a *= r;
+        }
+    }
+}
