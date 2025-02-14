9ceef8a8e773e8e93cc497a0ff14fd57cfab2e8b
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-14 22:41:59 +0530

    Infinite Loop virus

--- src/MasteringLoops/InfiniteLoop.java
+++ src/MasteringLoops/InfiniteLoop.java
@@ -3,7 +3,8 @@ package MasteringLoops;
 //infite loop
 public class InfiniteLoop {
     public static void main(String[] args) {
-            for(int i=0;i>0;i++)
+        for (int i = 0; i > 0; i++) {
             System.out.println(i);
+        }
     }
 }
