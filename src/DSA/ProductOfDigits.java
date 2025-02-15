85695bdbf8e7f73e5ae01abe7218614968a8e67b
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-15 22:43:35 +0530

    product of Digits

---
+++ src/DSA/ProductOfDigits.java
@@ -0,0 +1,19 @@
+package DSA;
+//Q1wap to print print product of digits of a given number
+//Q2 wap to print product of digits of a given number
+import java.util.Scanner;
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num=sc.nextInt();
+        int product = 1;
+        while (num > 0) {
+            int digit = num % 10; // Extract last digit
+            product *= digit; // Multiply with product
+            num /= 10; // Remove last digit
+        }
+        System.out.println("Product of digits: " + product);
+        }
+    }
+
