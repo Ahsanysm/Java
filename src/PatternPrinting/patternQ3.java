da510062cc02354650c4742a0ee0ee3943fe5abf
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-17 23:46:26 +0530

    Nums solid square pattern 2

---
+++ src/PatternPrinting/patternQ3.java
@@ -0,0 +1,22 @@
+package PatternPrinting;
+/*wap to print taking n as input
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+ */
+import java.util.Scanner;
+public class patternQ3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n=sc.nextInt();
+        for (int i = 1; i <= n ;i++) {
+            for (int j = 1; j <=4 ; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
