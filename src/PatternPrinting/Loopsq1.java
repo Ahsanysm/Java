26071ac436be9fee8f6290ab45c89c93f38f0483
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-17 22:58:57 +0530

    pattern printing q1

---
+++ src/PatternPrinting/Loopsq1.java
@@ -0,0 +1,15 @@
+package PatternPrinting;
+/*print the : taking no. of lines as input
+        ******
+        ******
+        ******
+*/
+
+public class Loopsq1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("*****");
+        }
+    }
+}
+
