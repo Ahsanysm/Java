1c6b835f1a0fa511b7873ba739919b1f2406d1a7
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-17 23:07:54 +0530

    pattern printing on users input

---
+++ src/PatternPrinting/UserBasedPatterns.java
@@ -0,0 +1,20 @@
+package PatternPrinting;
+//take from user no. of coloums and rows and then print pattern
+import java.util.Scanner;
+public class UserBasedPatterns {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows");
+        int m = sc.nextInt();
+        System.out.println("Enter no. of colums");
+        int n = sc.nextInt();
+        for (int i=1;i<=m;i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();//this gives next line for each loop
+        }
+    }
+}
