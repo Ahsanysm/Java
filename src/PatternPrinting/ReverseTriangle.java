e2c7bb7a4da4803a73b251e4804dc5f47a3fec6c
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-19 23:33:57 +0530

    reverse NumTriangle pattern

--- src/PatternPrinting/ReverseTriangle.java
+++ src/PatternPrinting/ReverseTriangle.java
@@ -13,7 +13,7 @@ public class ReverseTriangle {
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n + 1 - i; j++) {
-                System.out.print("* ");
+                System.out.print((char)(i+64)+" ");
             }
             System.out.println();
         }
