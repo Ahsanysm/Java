a71492727fe6c996eec54a57caf0ebc012b860ba
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-20 22:10:24 +0530

    AlphaNumeric reverse pattern

---
+++ src/PatternPrinting/AlphaNumericReversePattern.java
@@ -0,0 +1,27 @@
+package PatternPrinting;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+/* print TAKE N AS INPUT
+AAAAAA
+2222
+CCC
+44
+E
+*/
+import java.util.Scanner;
+public class AlphaNumericReversePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N");
+        int n=sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n+1-i ; j++) {
+                if(i%2==0) System.out.print(j+" ");
+                else System.out.print((char)(j+64)+" ");
+            }
+                System.out.println();
+        }
+
+    }
+}
