feb3abe46d2dc683250412ecba8066dbf0cb821a
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-02-20 22:13:43 +0530

    AlphaNumeric reverse pattern type 2nd

--- src/PatternPrinting/AlphaNumericReversePattern.java
+++ src/PatternPrinting/AlphaNumericReversePattern.java
@@ -17,11 +17,10 @@ public class AlphaNumericReversePattern {
         int n=sc.nextInt();
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <=n+1-i ; j++) {
-                if(i%2==0) System.out.print(j+" ");
-                else System.out.print((char)(j+64)+" ");
+                if(i%2==0) System.out.print(i+" ");
+                else System.out.print((char)(i+64)+" ");
             }
                 System.out.println();
         }
-
     }
 }
