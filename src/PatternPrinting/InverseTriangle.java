0b4ae197c9c83e53d6f5859052f9d66775ec69d5
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-02 10:30:39 +0530

    right triangle

--- src/PatternPrinting/InverseTriangle.java
+++ src/PatternPrinting/InverseTriangle.java
@@ -12,9 +12,9 @@ public class InverseTriangle {
         int n=sc.nextInt();
         for (int i = 0; i <=n ; i++) {
             for (int j = 0; j <=n ; j++) {
-                if (i + j <= n)
-                    System.out.print(" ");
-                else System.out.print("* ");
+                if ((i + j) > n)
+                    System.out.print("*"+" ");
+                else System.out.print(" "+" ");
             }
             System.out.println();
         }
