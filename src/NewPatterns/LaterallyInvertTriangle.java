3999464b52c6700d349372c209c36c5b32000188
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-08 11:02:59 +0530

    Invert triangle

--- src/NewPatterns/LaterallyInvertTriangle.java
+++ src/NewPatterns/LaterallyInvertTriangle.java
@@ -10,15 +10,21 @@ public class LaterallyInvertTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size");
-        int n= sc.nextInt();
-        for (int i = 0; i <=n ; i++) {
-            for (int j = 0; j <=n ; j++) {
-                if(i+j>n)
-                    System.out.print("*");
-                else System.out.print(" ");
-
+        int n = sc.nextInt();
+//        for (int i = 0; i <=n ; i++) {
+//            for (int j = 0; j <=n ; j++) {
+//                if(i+j>n)
+//                    System.out.print("*"+" ");
+//                else System.out.print(" "+" ");
+        for (int i = 1; i <= n; i++) {  //rows
+            for (int j = 1; j <= n - i; j++) {//spaces
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <= i; j++) { //stars
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
     }
 }
+
