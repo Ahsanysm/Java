1c59a6065b7d43ac698d2749aea16d2fa611ef46
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-08 10:38:38 +0530

    laterally inversion of triangle

---
+++ src/NewPatterns/LaterallyInvertTriangle.java
@@ -0,0 +1,24 @@
+package NewPatterns;
+/* print the laterally invert number
+     *
+    **
+   ***
+  ****
+ */
+import java.util.Scanner;
+public class LaterallyInvertTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size");
+        int n= sc.nextInt();
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 0; j <=n ; j++) {
+                if(i+j>n)
+                    System.out.print("*");
+                else System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
