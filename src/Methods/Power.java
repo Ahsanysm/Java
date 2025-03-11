2b02dacf7b2b857e892e2a5fac3d621f2d0814eb
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-11 17:58:05 +0530

    power of num

--- src/Methods/Power.java
+++ src/Methods/Power.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a");
-        int a=sc.nextInt();
-        System.out.println(" enter b");
-        int b=sc.nextInt();
-        int pow=1;
-        for (int i = 1; i <=b ; i++) {
-            pow*=a;
-        }
-        System.out.println(pow);
+        System.out.println("enter number");
+        double a=sc.nextDouble();
+        System.out.println(" enter exponent");
+        double b=sc.nextDouble();
+        System.out.println(Math.pow(a,b));
+//        int pow=1;
+//        for (int i = 1; i <=b ; i++) {
+//            pow *= a;
+//        }
+       // System.out.println(pow);
     }
 }
