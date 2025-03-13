4361405ee0662f188df51171a74e8c8e5971cf2b
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-13 22:44:36 +0530

    fun

---
+++ src/Methods/Yoyo.java
@@ -0,0 +1,23 @@
+package Methods;
+
+public class Yoyo {
+    public static void pehla(){
+        for (int i = 0; i <=10 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        System.out.println("Yo yo");
+        pehla();
+        System.out.println("Gian hain aap");
+        teesra();
+        dusra();
+    }
+    public static void teesra (){
+        System.out.println("Kaisa hai bhai");
+    }
+    public static void dusra(){
+        System.out.println("Alhamdulilah");
+    }
+}
